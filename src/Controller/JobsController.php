4bea49834acd84c52153ef8fe47392ff4c1222bb
Ophelie HORNN <hornn.ophelie@gmail.com>
2023-11-06 14:55:34 +0100

    Store token in cache, delete function, set env params

--- src/Controller/JobsController.php
+++ src/Controller/JobsController.php
@@ -5,23 +5,21 @@ namespace App\Controller;
 use Symfony\Contracts\Cache\CacheInterface;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\JsonResponse;
-use Symfony\Component\Config\Definition\Exception\Exception;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Contracts\Cache\ItemInterface;
 
 class JobsController extends AbstractController
 {
-
     public function getToken(): ?string
     {
-        $clientId = "d21517c83e5a991cf51cdf6d1d5a2037";
-        $clientSecret = "120dfc91d602665e74105d579adb71d0";
-        $api_url = 'https://api.jobijoba.com/v3/fr/login';
+        $clientId = $this->getParameter('client_id');
+        $clientSecret =  $this->getParameter('client_secret');
+        $api_url = $this->getParameter('api_url');
 
         // Récupération du token
         try {
             $curl = curl_init();
-            curl_setopt($curl, CURLOPT_URL, $api_url);
+            curl_setopt($curl, CURLOPT_URL, $api_url . 'login');
             curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
             curl_setopt($curl, CURLOPT_POST, true);
             curl_setopt($curl, CURLOPT_POSTFIELDS, json_encode(['client_id' =>
@@ -29,21 +27,37 @@ class JobsController extends AbstractController
             $response = json_decode(curl_exec($curl));
             curl_close($curl);
 
-            if ($response->code == 200) {
+            if ($response->code == 200 && isset($response->token)) {
                 return $response->token;
             }
-
             return null;
         } catch (\Exception $e) {
+
             return null;
         }
     }
 
-    #[Route('/', name: 'app_jobs')]
-    public function index(CacheInterface $tokenCache)
+    #[Route('/delete', name: 'delete_token')]
+    public function deleteToken(CacheInterface $cacheInterface): JsonResponse
     {
-        $token = $this->getToken();
+        if ($cacheInterface->hasItem('api_token')) {
+            $cacheInterface->deleteItem('api_token');
+            return $this->json('Token supprimé avec succès du cache.');
+        } else {
+            return $this->json('Le token n\'existe pas dans le cache.');
+        }
+    }
 
-        dd($token);
+
+    #[Route('/', name: 'home')]
+    public function index(CacheInterface $cache): JsonResponse
+    {
+
+        $token = $cache->get('api_token', function (ItemInterface $item) {
+            $item->expiresAfter(3600);
+            return $this->getToken();
+        });
+
+        return $this->json($token);
     }
 }
